91c8f22caf1bfedf47deba23dae4fe6bf9c018e4
unknown <patryk.piotrowski@edu.uekat.pl>
2021-11-07 15:48:05 +0100

    Zadanie_2_b_1

---
+++ zad_2_b_1.py
@@ -0,0 +1,9 @@
+def print_number(tabela2):
+    wartosc = []
+    for number in tabela2:
+        wartosc.append(number * 2)
+    return wartosc
+
+
+numery = [1,2,3,4,5]
+print(print_number(numery))
