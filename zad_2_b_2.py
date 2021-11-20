25788265f2081a42ef6f19033b55a86dbd336b9d
PatrykPiotrowski1337 <patryk.piotrowski@edu.uekat.pl>
2021-11-20 12:31:56 +0100

    Update

---
+++ zad_2_b_2.py
@@ -0,0 +1,6 @@
+def printNumbersList(matrix):
+    return [i * 2 for i in matrix]
+
+
+numbers = [1, 2, 3, 4, 5]
+print(printNumbersList(numbers))
---
+++ zad_2_c.py
@@ -0,0 +1,4 @@
+numery = [i for i in range(10)]
+for i in range(len(numery)):
+    if numery[i] % 2 == 0:
+        print(i)
